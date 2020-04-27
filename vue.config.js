2d406bd10de515286a693530ba2900ca414021c6
hejialiang <984714129@qq.com>
2020-04-27 19:52:12 +0800

    添加analyz
    Signed-off-by: hejialiang <984714129@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,16 @@ const env = process.env
 const isProd = process.env.NODE_ENV === 'production'
 const deepConsole = process.env.VUE_APP_SERVEN === 'PROD'
 const TerserPlugin = require('terser-webpack-plugin')
+let BundleAnalyzerPlugin
+if (process.env.npm_config_report) {
+  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+}
 module.exports = {
   publicPath: isProd ? `${env.WEBPACK_PATH}` : '/',
   configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin()
+    ],
     externals: isProd ? {
       'vue': 'Vue',
       'vue-router': 'VueRouter',
