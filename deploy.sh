b25ce9ba62d0a00c549346edc06b6ffc257c7e9f
hejialiang <984714129@qq.com>
2020-06-20 14:30:44 +0800

    添加构建脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env sh
+#当发生错误中止脚本
+set -e
+# cd 到构建输出的目录下
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+# 这里的access_token是travis设置的环境变量
+git push -f https://${access_token}@github.com/hejialianghe/hejialianghe.github.io master
+cd -  # 返回上一层
