9f2268b2476d03bd0a015a9b4c0581ab7e6dd8e2
hejialiang <984714129@qq.com>
2020-09-22 22:29:12 +0800

    测试2

--- generator/index.js
+++ generator/index.js
@@ -1,23 +1,6 @@
 module.exports = (api, options, rootOptions) => {
  
-    if(options.application=== 'pc') {
-        api.extendPackage({
-            dependencies: {
-                "amfe-flexible": "^2.2.1",
-                "vant": "^2.0.9"
-            },
-            devDependencies: {
-                "postcss-pxtorem": "^4.0.1",
-            }
-        })
 
-    }else {
-        api.extendPackage({
-            dependencies: {
-              'element-ui': '^2.8.2'
-            }
-          });
-    }
-
+   console.log('测试');
     api.render('./template')
 }
