141d18292e765fb84501563a917388040d5ed191
hejialiang <984714129@qq.com>
2020-09-27 21:23:25 +0800

    测试

---
+++ prompts1.js
@@ -0,0 +1,12 @@
+module.exports=[
+  {
+      name:'application',
+      type:'list',
+      message:'请选择你要生成的项目类型',
+      choices:[
+          {name:'pc',value:'pc'},
+          {name:'mobile',value:'mobile'}
+      ],
+      defalut:'mobile-web'
+  }
+]
