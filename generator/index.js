41cc0ec2bafbd0b608ea899b9dcbe0d528caae0d
hejialiang <984714129@qq.com>
2020-09-29 15:27:54 +0800

    测试

--- generator/index.js
+++ generator/index.js
@@ -24,33 +24,36 @@ module.exports = (api, options, rootOptions) => {
                 "vuex-persistedstate": "^2.5.4"
             },
             "devDependencies": {
+                "@babel/plugin-transform-runtime": "^7.11.0",
                 "@vue/cli-plugin-babel": "~4.5.0",
                 "@vue/cli-plugin-eslint": "~4.5.0",
                 "@vue/cli-plugin-router": "~4.5.0",
                 "@vue/cli-plugin-vuex": "~4.5.0",
                 "@vue/cli-service": "~4.5.0",
                 "@vue/eslint-config-standard": "^5.1.2",
+                "add-asset-html-webpack-plugin": "^3.1.3",
                 "babel-eslint": "^10.1.0",
+                "babel-plugin-import": "^1.13.0",
+                "babel-plugin-lodash": "^3.3.4",
+                "compression-webpack-plugin": "^4.0.0",
                 "eslint": "^6.7.2",
                 "eslint-plugin-import": "^2.20.2",
                 "eslint-plugin-node": "^11.1.0",
                 "eslint-plugin-promise": "^4.2.1",
                 "eslint-plugin-standard": "^4.0.0",
                 "eslint-plugin-vue": "^6.2.2",
-                "sass": "^1.26.5",
-                "sass-loader": "^8.0.2",
-                "vue-template-compiler": "^2.6.11",
-                "webpack-bundle-analyzer": "^3.8.0",
-                "terser-webpack-plugin": "^1.4.1",
-                "uglifyjs-webpack-plugin": "^2.2.0",
+                "lint-staged": "^9.5.0",
                 "lodash": "^4.17.15",
                 "lodash-webpack-plugin": "^0.11.5",
-                "@babel/plugin-transform-runtime": "^7.11.0",
-                "add-asset-html-webpack-plugin": "^3.1.3",
-                "compression-webpack-plugin": "^4.0.0"
+                "sass": "^1.26.5",
+                "sass-loader": "^8.0.2",
+                "terser-webpack-plugin": "^1.4.1",
+                "uglifyjs-webpack-plugin": "^2.2.0",
+                "vue-template-compiler": "^2.6.11",
+                "webpack-bundle-analyzer": "^3.8.0"
          }
     })
-    if(options.application=== 'pc') {
+    if(options.application=== 'mobile') {
         api.extendPackage({
             dependencies: {
                 "amfe-flexible": "^2.2.1",
