fa415c3766f998f1b3a116687704b2add6f61183
hejialiang <984714129@qq.com>
2020-09-29 22:09:45 +0800

    测试

--- generator/template/babel.config.js
+++ generator/template/babel.config.js
@@ -1,37 +1,37 @@
 
 
-<%_ if(options.application === 'mobile') { _%>
-  module.exports = {
-    presets: [
-      ['@vue/cli-plugin-babel/preset', {
-        useBuiltIns: 'usage'
-      }]
-    ],
-    plugins: [
-      'lodash',
-      ['@babel/plugin-transform-runtime'],
-      [
-        'import',
-        {
-          'libraryName': 'vant',
-          'libraryDirectory': 'es',
-          'style': true
-        }
-      ]
-    ]
-  }
+// <%_ if(options.application === 'mobile') { _%>
+//   module.exports = {
+//     presets: [
+//       ['@vue/cli-plugin-babel/preset', {
+//         useBuiltIns: 'usage'
+//       }]
+//     ],
+//     plugins: [
+//       'lodash',
+//       ['@babel/plugin-transform-runtime'],
+//       [
+//         'import',
+//         {
+//           'libraryName': 'vant',
+//           'libraryDirectory': 'es',
+//           'style': true
+//         }
+//       ]
+//     ]
+//   }
  
-<%_ } else _%>
-module.exports = {
-  presets: [
-    ['@vue/cli-plugin-babel/preset', {
-      useBuiltIns: 'usage'
-    }]
-  ],
-  plugins: [
-    'lodash',
-    ['@babel/plugin-transform-runtime']
-  ]
-}
-<%_ } _%}
+// <%_ } else _%>
+// module.exports = {
+//   presets: [
+//     ['@vue/cli-plugin-babel/preset', {
+//       useBuiltIns: 'usage'
+//     }]
+//   ],
+//   plugins: [
+//     'lodash',
+//     ['@babel/plugin-transform-runtime']
+//   ]
+// }
+// <%_ } _%}
 
